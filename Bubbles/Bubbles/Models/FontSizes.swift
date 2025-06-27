f01ff9d3a5bfe6bb58d1145854bfafcec30778af
jia-xinnnn <167150509+jia-xinnnn@users.noreply.github.com>
2025-06-27 17:29:57 +0800

    Set new font sizes

--- Bubbles/Bubbles/Models/FontSizes.swift
+++ Bubbles/Bubbles/Models/FontSizes.swift
@@ -9,4 +9,14 @@ import Foundation
 
 enum FontSizes: Int {
     case largeTitle = 80
+    case title = 66
+    case title2 = 52
+    case title3 = 47
+    case headline = 32
+    case body = 40
+    case callout = 38
+    case subheadline = 35
+    case footnote = 31
+    case caption = 28
+    case caption2 = 26
 }
